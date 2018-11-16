8a9b575d77df2554b7f5c4ba6ac60a76549c23d9
Davis too <davis.too@strathmore.edu>
2018-11-16 17:30:31 +0300

    almost done with api

--- api/api.py
+++ api/api.py
@@ -10,9 +10,10 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/api'
+
 db = SQLAlchemy(app)
 
-@app.route("/")
+@app.route("/v1")
 def hello():
     return jsonify({"about": "Welcome to sendit api"})
 
@@ -22,7 +23,18 @@ class User(db.Model):
     name = db.Column(db.String(50))
     password = db.Column(db.String(80))
     admin = db.Column(db.Boolean)
+class Parcel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    source = db.Column(db.String(50))
+    current_location = db.Column(db.String(50))
+    destination = db.Column(db.String(50))
+    status = db.Column(db.String(50))
+    user_id = db.Column(db.Integer)
 
+
+# function to check if the jwt tokens are valid and prefent unauthorized access to the api
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs ):
@@ -45,9 +57,10 @@ def token_required(f):
 
     return decorated
 
-
-@app.route('/user', methods=['GET'])
+# user routes and respective functions
+@app.route('/v1/user', methods=['GET'])
 @token_required
+# gets all users from the databse and returns a json object
 def get_all_users(current_user):
     if not current_user.admin:
         return jsonify({'message': 'you are not authorized'})
@@ -67,8 +80,10 @@ def get_all_users(current_user):
 
     return jsonify({"users": output})
 
-@app.route('/user/<public_id>', methods=['GET'])
+@app.route('/v1/user/<public_id>', methods=['GET'])
 @token_required
+# gets only one user from the database and puts his/her info in a json object
+
 def get_one_user(current_user, public_id):
     if not current_user.admin:
         return jsonify({'message': 'you are not authorized'})
@@ -87,8 +102,9 @@ def get_one_user(current_user, public_id):
     user_data['admin'] = user.admin
     return jsonify({"user": user_data})
 
-@app.route('/user', methods=['POST'])
+@app.route('/v1/user', methods=['POST'])
 @token_required
+# creates a new user in the databse
 def create_user(current_user):
     if not current_user.admin:
         return jsonify({'message': 'you are not authorized'})
@@ -103,8 +119,8 @@ def create_user(current_user):
     return jsonify({'message': 'New user Created'})
 
 
-
-@app.route('/user/<public_id>', methods=['PUT'])
+# updates a user to admin status
+@app.route('/v1/user/<public_id>', methods=['PUT'])
 @token_required
 def promote_user(current_user, public_id):
     if not current_user.admin:
@@ -122,8 +138,9 @@ def promote_user(current_user, public_id):
 
     return jsonify({'message': 'User is now an admin'})
 
-@app.route('/user/<public_id>', methods=['DELETE'])
+@app.route('/v1/user/<public_id>', methods=['DELETE'])
 @token_required
+# deletes user from the database
 def delete_user(current_user, public_id):
     if not current_user.admin:
         return jsonify({'message': 'you are not authorized'})
@@ -135,8 +152,88 @@ def delete_user(current_user, public_id):
     db.session.delete(user)
     db.session.commit()
     return jsonify({'message': 'User deleted successfully'})
+    
 
-@app.route('/login')
+# start the parcel routes 
+@app.route('/v1/parcel')
+@token_required
+# function for the admin to get all the parcels
+def get_all_parcels(current_user):
+    if not current_user.admin:
+        return jsonify({'message': 'you are not authorized'})
+
+    parcels = Parcel.query.all()
+
+    output = []
+
+    for parcel in parcels:
+        parcel_data = {}
+        parcel_data['public_id'] = parcel.public_id
+        parcel_data['name'] = parcel.name
+        parcel_data['source'] = parcel.source
+        parcel_data['destination'] = parcel.destination
+        parcel_data['status'] = parcel.status
+        parcel_data['user_id'] = parcel.user_id
+        
+        output.append(parcel_data)
+
+    return jsonify({"users": output})
+
+
+
+
+@app.route('/v1/parcel', methods=['POST'])
+
+def create_parcel(current_user):
+    user = current_user.id
+    data  = request.get_json(force=True)
+
+    parcel = Parcel(public_id = str(uuid.uuid4()), name=data['name'], source=data['source'], destination=data['destination'], status='started', user_id=user)
+    db.session.add(parcel)
+    db.session.commit()
+
+    return jsonify({'message': 'Parcel order created successfully'})
+
+@app.route('/v1/parcel/<public_id>/destination', methods=['PUT'])
+@token_required
+def change_destination(current_user, public_id):
+   
+
+    parcel = Parcel.query.filter_by(public_id=public_id).first()
+    if not parcel:
+        return jsonify({'message': 'No Parcel found'})
+    
+    data  = request.get_json(force=True)
+    parcel.destination = data['destination']
+    db.session.commit()
+
+
+
+    return jsonify({'message': 'Destination changed successully'})
+
+
+
+@app.route('/v1/parcel/<public_id>', methods=['PUT'])
+@token_required
+def edit_parcel(current_user, public_id):
+    if not current_user.admin:
+        return jsonify({'message': 'you are not authorized'})
+
+
+    parcel = Parcel.query.filter_by(public_id=public_id).first()
+    if not parcel:
+        return jsonify({'message': 'No Parcel found'})
+    
+    data  = request.get_json(force=True)
+    parcel.status = data['status']
+    parcel.current_location = data['current_location']
+    db.session.commit()
+
+
+
+    return jsonify({'message': 'Order Successfully edited'})
+
+@app.route('/v1/login')
 def login():
     auth = request.authorization
 
